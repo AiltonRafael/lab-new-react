01931d2b29b75f3280ea02baa665f202ee4cd84c
AiltonRafael <ailtonrafaeldiasjr@gmail.com>
2021-11-10 19:52:40 -0300

    updated

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Button } from '@chakra-ui/button';
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Box } from '@chakra-ui/layout';
+import React, {useState} from 'react';
 
 export default function FormComponent() {
-    return (
-        <div>
+    const [bgInputColorEmail, setBgInputColorEmail] = useState()
+    const [emailValue, setEmailValue] = useState()
+
+    const [bgInputColorPassword, setBgInputColorPassword] = useState()
+
+    function handleColor(type, value){
+        if(type === 'email'){
+                setBgInputColorEmail('red')
+                if(!emailValue)
+                    {
+                        setBgInputColorEmail('red')
+                    }
             
-        </div>
+        }
+    }
+    
+    return (
+    <Box 
+    mt='20px'
+    d='flex'
+    flexDirection='column'
+    alignItems='center'
+    justifyContent='center'
+    textAlign='center'
+    gridGap='10px'
+    >
+        <FormControl id="first-name" isRequired>
+            <Input borderColor={bgInputColorEmail} placeholder="Email" type='email' value={emailValue} onChange={() => handleColor('email', emailValue)}/>
+            <Input borderColor={bgInputColorPassword} placeholder="Password" type='password' onChange={() => handleColor('password')} />
+            <Input placeholder="First name" />
+        </FormControl>
+        <Button 
+        w='200px'
+        bg='blue'
+        color='white'
+        > 
+            Submit 
+        </Button>
+    </Box>
     )
 }
